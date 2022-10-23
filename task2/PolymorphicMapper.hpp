d54d094a46384a46c8ab1cd45de8ec5e783ac944
Vladimir Makarov <makarov.vladimir@phystech.edu>
2022-10-23 11:07:41 +0300

    add solution

--- task2/PolymorphicMapper.hpp
+++ task2/PolymorphicMapper.hpp
@@ -8,5 +8,17 @@ struct Mapping;
 
 template <class Base, class Target, class... Mappings>
 struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+  static std::optional<Target> map(const Base&) {
+    return std::nullopt;
+  }
+};
+
+template <class Base, class Target, class From, auto target, class... Mappings>
+struct PolymorphicMapper<Base, Target, Mapping<From, target>, Mappings...> {
+  static std::optional<Target> map(const Base& object) {
+    if (dynamic_cast<const From*>(&object)) {
+      return {target};
+    }
+    return PolymorphicMapper<Base, Target, Mappings...>::map(object);
+  }
 };
