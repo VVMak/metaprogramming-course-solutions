ce55b93fd91471ef242e48956e70e320ba74a577
Vladimir Makarov <makarov.vladimir@phystech.edu>
2022-10-23 11:51:26 +0300

    fix nocompile

--- task2/PolymorphicMapper.hpp
+++ task2/PolymorphicMapper.hpp
@@ -3,6 +3,9 @@
 #include <optional>
 
 
+template<typename>
+constexpr bool depFalse = false;
+
 template <class From, auto target>
 struct Mapping;
 
@@ -13,9 +16,9 @@ struct PolymorphicMapper {
   }
 };
 
-template <class Base, class Target, class From, auto target, class... Mappings>
+template <class Base, class Target, class From, class MappingTarget, MappingTarget target, class... Mappings>
 struct PolymorphicMapper<Base, Target, Mapping<From, target>, Mappings...> {
-  static std::optional<Target> map(const Base& object) {
+  static std::optional<Target> map(const Base& object) requires(std::same_as<Target, MappingTarget>) {
     if (dynamic_cast<const From*>(&object)) {
       return {target};
     }
